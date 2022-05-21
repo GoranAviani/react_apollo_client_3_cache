1b004729cf9895e143aee225c4eff064f0cade0f
GoranAviani <goranaviani@gmail.com>
2022-05-21 21:19:32 +0200

    react-weather

--- src/ components/IndexPage/IndexPage.js
+++ src/ components/IndexPage/IndexPage.js
@@ -1,23 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 import Navigation from "../Navigation/Navigation";
 import DefaultCities from "../DefaultCities/DefaultCities";
 import Footer from "../Footer/Footer";
+import SearchedCity from "../SearchedCity/SearchedCity";
 
 import './IndexPage.css'
 
 const IndexPage = () => {
-
+    const [showSearchResult, setShowSearchResult] = useState(false)
+    const [searchedCityInfo, setSearchedCityInfo] = useState("searchedCityInfo info")
     const searchCityAPI = (userInput) => {
         console.log("index")
         console.log(userInput)
+        setShowSearchResult(true)
     }
 
 
     return(
+
         <div className="indexPage">
             <Navigation searchCityAPI={searchCityAPI}/>
-            <DefaultCities/>
+            (showSearchResult ? <SearchedCity searchedCityInfo = {searchedCityInfo}/> :<DefaultCities/>)
             <Footer/>
         </div>
     )
