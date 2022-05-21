934b2439404cb6ade74b76b23265d3ec40ebb392
GoranAviani <goranaviani@gmail.com>
2022-05-21 21:47:25 +0200

    react-weather

--- src/ components/SearchedCity/SearchedCity.js
+++ src/ components/SearchedCity/SearchedCity.js
@@ -7,17 +7,17 @@ const SearchedCity = ({searchedCityDetails}) => {
         <div className="searchedCity">
             <div className="searchedCityMainDetails">
 
-            <div className="currentTemperature">
-                <div className="currentTemperatureTitle">Temperature</div>
-                <div className="currentTemperatureNumber">current temp</div>
+                <div className="currentTemperature">
+                    <div className="currentTemperatureTitle">Temperature</div>
+                    <div className="currentTemperatureNumber">current temp</div>
+                </div>
+                <div className="citsearchedCityInfoName">
+                    city name {searchedCityDetails}
+                </div>
+                <div>
+                    <div className="currentWeatherIcon">weather Icon</div>
+                </div>
             </div>
-            <div className="citsearchedCityInfoName">
-                city name {searchedCityDetails}
-            </div>
-            <div>
-                <div className="currentWeatherIcon">weather Icon</div>
-            </div>
-</div>
             <div className="searchedCityOtherDetails">
                 <div>
                     <div className="lowestTemperatureTitle">Daily lowest</div>
