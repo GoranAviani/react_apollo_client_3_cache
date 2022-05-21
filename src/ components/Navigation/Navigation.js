5777cdee88fee7d2ad834739069a137f66d353c2
GoranAviani <goranaviani@gmail.com>
2022-05-21 14:11:20 +0200

    react-weather

--- src/ components/Navigation/Navigation.js
+++ src/ components/Navigation/Navigation.js
@@ -1,10 +1,19 @@
 import React from 'react';
 
+import {Button} from "@mui/material";
+
+import './Navigation.css'
+
 const Navigation = () =>{
 
     return(
-        <div>
-            Nav
+        <div class="Navigation">
+            <Button variant="outlined">
+                Login
+            </Button>
+            <Button variant="outlined">
+                Register
+            </Button>
         </div>
     )
 }
