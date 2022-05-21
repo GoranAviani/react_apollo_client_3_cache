8f16821d046d94ef581cb0823797c7c664fa7d79
GoranAviani <goranaviani@gmail.com>
2022-05-21 14:33:24 +0200

    react-weather

--- src/ components/Search/Search.js
+++ src/ components/Search/Search.js
@@ -5,11 +5,15 @@ import {TextField} from "@mui/material";
 const Search = () => {
     const [userSearch, setUserSearch] = useState("")
 
+    const searchHandler = (e) => {
+        setUserSearch(e.target.value)
+        console.log(userSearch)
 
+    }
 
     return(
         <div>
-            <TextField id="standard-basic" label="Search" variant="standard" onChange={searchHandler}/>
+            <TextField id="standard-basic" label="Search" variant="standard" value={userSearch} onChange={searchHandler}/>
         </div>
     )
 }
