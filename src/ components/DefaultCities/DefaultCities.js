23fc176dee5cbc31deb35db8280ffa24b6bfc9d3
GoranAviani <goranaviani@gmail.com>
2022-05-21 18:19:32 +0200

    react-weather

--- src/ components/DefaultCities/DefaultCities.js
+++ src/ components/DefaultCities/DefaultCities.js
@@ -1,10 +1,65 @@
 import React from "react";
+import './DefaultCities.css'
+import {Button} from "@mui/material";
 
 const DefaultCities = () => {
 
-    return(
-        <div>
-            def cities
+    return (
+        <div className="defaultCities">
+            <div className="defaultCity">
+                <div className="defaultCityName">Split</div>
+                <div className="defualtCityIcon">
+                    Ikona
+                </div>
+                <div className="defaultCityTemperature">
+                    18C
+                </div>
+                <Button>Open</Button>
+            </div>
+
+
+
+            <div className="defaultCity">
+                <div className="defaultCityName">Split</div>
+                <div className="defualtCityIcon">
+                    Ikona
+                </div>
+                <div className="defaultCityTemperature">
+                    18C
+                </div>
+                <Button>Open</Button>
+            </div><div className="defaultCity">
+                <div className="defaultCityName">Split</div>
+                <div className="defualtCityIcon">
+                    Ikona
+                </div>
+                <div className="defaultCityTemperature">
+                    18C
+                </div>
+                <Button>Open</Button>
+            </div><div className="defaultCity">
+                <div className="defaultCityName">Split</div>
+                <div className="defualtCityIcon">
+                    Ikona
+                </div>
+                <div className="defaultCityTemperature">
+                    18C
+                </div>
+                <Button>Open</Button>
+            </div><div className="defaultCity">
+                <div className="defaultCityName">Split</div>
+                <div className="defualtCityIcon">
+                    Ikona
+                </div>
+                <div className="defaultCityTemperature">
+                    18C
+                </div>
+                <Button>Open</Button>
+            </div>
+
+
+
+
         </div>
     )
 }
