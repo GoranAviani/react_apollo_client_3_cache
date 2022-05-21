ee45dbc60280612bf3ff0bcaf861d49938203b87
GoranAviani <goranaviani@gmail.com>
2022-05-21 14:18:47 +0200

    react-weather

---
+++ src/ components/LoginRegister/LoginRegister.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Button} from "@mui/material";
+
+
+const LoginRegister = () => {
+
+    return(
+        <div>
+            <Button variant="outlined">
+                Login
+            </Button>
+            <Button variant="outlined">
+                Register
+            </Button>
+        </div>
+    )
+}
+
+
+export default LoginRegister;
