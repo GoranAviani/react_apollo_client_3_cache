569c3a8bd206a91cdbc06513895df6ea33867b23
GoranAviani <goranaviani@gmail.com>
2022-05-21 18:06:45 +0200

    react-weather

--- src/ components/IndexPage/IndexPage.js
+++ src/ components/IndexPage/IndexPage.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import Navigation from "../Navigation/Navigation";
 import DefaultCities from "../DefaultCities/DefaultCities";
+import Footer from "../Footer/Footer";
 
 const IndexPage = () => {
     return(
         <React.Fragment>
             <Navigation/>
             <DefaultCities/>
+            <Footer/>
         </React.Fragment>
     )
 }
