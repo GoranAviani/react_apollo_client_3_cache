99fda79c77e2c780e80a1bdb618ef0a88ba6e0b7
GoranAviani <goranaviani@gmail.com>
2022-05-21 13:34:14 +0200

    react-weather

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from 'react';
-import './App.css';
+
 import Navigation from './ components/Navigation/Navigation'
 import DefaultCities from "./ components/DefaultCities/DefaultCities";
 
-function App() {
-  return (
-      <React.Fragment>
-        <Navigation/>
-        <DefaultCities/>
-      </React.Fragment>
+import './App.css';
 
-  );
+
+function App() {
+    return (
+        <React.Fragment>
+            <Navigation/>
+            <DefaultCities/>
+        </React.Fragment>
+
+    );
 }
 
 export default App;
