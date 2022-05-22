6aea5e24453da25a66cdfb933c06b94980654c15
GoranAviani <goranaviani@gmail.com>
2022-05-22 21:55:11 +0200

    react-weather

--- src/ components/Dashboard/Dashboard.js
+++ src/ components/Dashboard/Dashboard.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+import {Link} from "react-router-dom";
+
+
 const Dashboard = () => {
     return(
         <div>
+        <Link to="/">go home page</Link>
+
             this is dashboard
         </div>
     )
