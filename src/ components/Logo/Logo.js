298a3d0ebcf261bdd60451a6ca015cc7426bcfbe
GoranAviani <goranaviani@gmail.com>
2022-05-22 21:36:06 +0200

    react-weather

--- src/ components/Logo/Logo.js
+++ src/ components/Logo/Logo.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import './Logo.css'
+import {Link, Route, Routes} from "react-router-dom";
+
+import IndexPage from "../IndexPage/IndexPage";
+import Dashboard from "../Dashboard/Dashboard";
 
 const Logo = () => {
     return (
         <div className="logo">
-            This is logo
+            <Link to="/dashboard">Logo</Link>
         </div>
     )
 }
