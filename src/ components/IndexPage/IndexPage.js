d220114bffb803623263d654939c190f3169326c
goranaviani <goranaviani@gmail.com>
2022-09-10 14:29:04 +0200

    setup

--- src/ components/IndexPage/IndexPage.js
+++ src/ components/IndexPage/IndexPage.js
@@ -23,7 +23,8 @@ const IndexPage = () => {
     }
 
     const searchCityAPIHandler = async (cityName) => {
-        /* check input */             setShowHomePage(false)
+        /* check input */
+        setShowHomePage(false)
 
         await useSearchCityAPI(cityName, setSearchedCityDetails, setIsLoading)
 
