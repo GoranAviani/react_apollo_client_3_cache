6dfeb53313624d7d4fcb8ecd1efdae3e79c31738
goranaviani <goranaviani@gmail.com>
2022-09-15 22:27:51 +0200

    setup

--- src/hooks/axiosClient.js
+++ src/hooks/axiosClient.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 
 const createAxiosClient =  () => {
-       // return axios.create({baseURL: "https://weather.deta.dev"})
-                return axios.create({baseURL: "http://127.0.0.1:8000/"})
+       return axios.create({baseURL: "https://weather.deta.dev"})
+       //         return axios.create({baseURL: "http://127.0.0.1:8000/"})
 
 
 }
