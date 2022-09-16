76b034db535a8e6cdaebb3fa2b1dda266fb3b188
goranaviani <goranaviani@gmail.com>
2022-09-16 17:06:13 +0200

    setup

--- src/hooks/axiosClient.js
+++ src/hooks/axiosClient.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const WEATHER_MICROSERVICE = "https://weather.deta.dev"
 
 const createAxiosClient =  () => {
-       return axios.create({baseURL: "https://weather.deta.dev"})
+       return axios.create({baseURL: WEATHER_MICROSERVICE})
        //         return axios.create({baseURL: "http://127.0.0.1:8000/"})
 
 
