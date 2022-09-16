a572d06da86aab6f36c9e0a4196c4d5143a1f8ef
goranaviani <goranaviani@gmail.com>
2022-09-16 22:43:30 +0200

    setup

--- src/hooks/api/getCurrentWeather.js
+++ src/hooks/api/getCurrentWeather.js
@@ -1,9 +1,21 @@
+const getCurrentWeather = (axiosClient, accessKey, cityName, setIsLoading, setSearchedCityDetails) => {
 
+    if (accessKey) {
+        const config1 = {
+            headers: {Authorization: `Bearer ${accessKey}`}
+        };
+        axiosClient.get(`/current/${cityName}`, config1).then((response) => {
+            setSearchedCityDetails(response)
+            //await new Promise(resolve => setTimeout(resolve, 1000));
+            setIsLoading(false)
+        }).catch(function (error) {
+            setIsLoading(false)
+            //TODO ovde pokazat error useru
+            console.log(error.toJSON())
+        })
 
-const getCurrentWeather = () => {
-
-
+    }
 
 }
 
-export default getCurrentWeather()
+export default getCurrentWeather;
--- src/hooks/api/useSearchCityAPI.js
+++ src/hooks/api/useSearchCityAPI.js
@@ -1,5 +1,6 @@
 import createAxiosClient from "./axiosClient";
 import getToken from "./getToken";
+import getCurrentWeather from "./getCurrentWeather";
 import {useEffect, useState} from "react";
 
 export const useSearchCityAPI = (cityName, setSearchedCityDetails, setIsLoading) => {
@@ -11,23 +12,7 @@ export const useSearchCityAPI = (cityName, setSearchedCityDetails, setIsLoading)
         setIsLoading(true)
         const accessKey = getToken(axiosClient)
         //await new Promise(resolve => setTimeout(resolve, 1000));
-
-        if (accessKey) {
-            getCurrentWeather()
-            const config1 = {
-                headers: {Authorization: `Bearer ${accessKey}`}
-            };
-            axiosClient.get(`/current/${cityName}`, config1).then((response) => {
-                setSearchedCityDetails(response)
-                //await new Promise(resolve => setTimeout(resolve, 1000));
-                setIsLoading(false)
-            }).catch(function (error) {
-                setIsLoading(false)
-                //TODO ovde pokazat error useru
-                console.log(error.toJSON())
-            })
-
-        }
+        getCurrentWeather(axiosClient, accessKey, cityName, setIsLoading, setSearchedCityDetails)
 
 
     }
