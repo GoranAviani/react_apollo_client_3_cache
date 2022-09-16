f860f675801b42ed6132a31c01149ae2d5230ec0
goranaviani <goranaviani@gmail.com>
2022-09-16 22:29:11 +0200

    setup

--- src/hooks/api/getToken.js
+++ src/hooks/api/getToken.js
@@ -1,27 +1,27 @@
 const FormData = require("form-data");
 
-const getToken = () => {
+const getToken = (axiosClient) => {
 
-var FormData = require('form-data');
-var data = new FormData();
-data.append('username', 'johndoe');
-data.append('password', 'secret');
+    var FormData = require('form-data');
+    var data = new FormData();
+    data.append('username', 'johndoe');
+    data.append('password', 'secret');
 //   url: 'http://127.0.0.1:8000/token',
-var config = {
-  method: 'post',
-  url: '/token',
-  data : data
-};
+    var config = {
+        method: 'post',
+        url: '/token',
+        data: data
+    };
 
-const accessKey = axiosClient(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-  return (response.data.access_token)
-})
-.catch(function (error) {
-  console.log(error);
-});
-
+    axiosClient(config)
+        .then(function (response) {
+            console.log(JSON.stringify(response.data));
+            console.log(response.data.access_token)
+            return response.data.access_token
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
 
 }
 
