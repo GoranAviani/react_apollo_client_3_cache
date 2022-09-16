216470471077754fb0c2757811d950d72aea2b2a
goranaviani <goranaviani@gmail.com>
2022-09-16 22:49:11 +0200

    setup

--- src/hooks/api/getCurrentWeather.js
+++ src/hooks/api/getCurrentWeather.js
@@ -1,20 +1,18 @@
 const getCurrentWeather = (axiosClient, accessKey, cityName, setIsLoading, setSearchedCityDetails) => {
 
-    if (accessKey) {
-        const config1 = {
-            headers: {Authorization: `Bearer ${accessKey}`}
-        };
-        axiosClient.get(`/current/${cityName}`, config1).then((response) => {
-            setSearchedCityDetails(response)
-            //await new Promise(resolve => setTimeout(resolve, 1000));
-            setIsLoading(false)
-        }).catch(function (error) {
-            setIsLoading(false)
-            //TODO ovde pokazat error useru
-            console.log(error.toJSON())
-        })
+    const config1 = {
+        headers: {Authorization: `Bearer ${accessKey}`}
+    };
+    axiosClient.get(`/current/${cityName}`, config1).then((response) => {
+        setSearchedCityDetails(response)
+        //await new Promise(resolve => setTimeout(resolve, 1000));
+        setIsLoading(false)
+    }).catch(function (error) {
+        setIsLoading(false)
+        //TODO ovde pokazat error useru
+        console.log(error.toJSON())
+    })
 
-    }
 
 }
 
