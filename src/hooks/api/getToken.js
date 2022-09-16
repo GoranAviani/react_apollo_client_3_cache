8340ac386536366c05f976083c5d11d9a4e6b4e8
goranaviani <goranaviani@gmail.com>
2022-09-16 22:50:11 +0200

    setup

--- src/hooks/api/getToken.js
+++ src/hooks/api/getToken.js
@@ -2,12 +2,11 @@ const FormData = require("form-data");
 
 const getToken = (axiosClient) => {
 
-    var FormData = require('form-data');
-    var data = new FormData();
+    const data = new FormData();
     data.append('username', 'johndoe');
     data.append('password', 'secret');
 //   url: 'http://127.0.0.1:8000/token',
-    var config = {
+    const config = {
         method: 'post',
         url: '/token',
         data: data
