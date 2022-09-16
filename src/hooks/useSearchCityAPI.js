1c0494b8cd4421f8fe6f83765b767322d947957a
goranaviani <goranaviani@gmail.com>
2022-09-16 17:07:22 +0200

    setup

--- src/hooks/useSearchCityAPI.js
+++ src/hooks/useSearchCityAPI.js
@@ -35,7 +35,22 @@ var config = {
   data : data
 };
 
-        axiosClient.get(`/current/${cityName}`, formData).then((response)=> {
+axios(config)
+.then(function (response) {
+  console.log(JSON.stringify(response.data));
+})
+.catch(function (error) {
+  console.log(error);
+});
+
+
+var data1 = new FormData();
+data1.append('access_token', 'johndoe');
+data1.append('token_type', 'bearer');
+const config1 = {
+    headers: { Authorization: `Bearer johndoe` }
+};
+        axiosClient.get(`/current/${cityName}`, config1).then((response)=> {
             setSearchedCityDetails(response)
             //await new Promise(resolve => setTimeout(resolve, 1000));
             setIsLoading(false)
