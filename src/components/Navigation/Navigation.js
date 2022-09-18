5c5c52c27df5ae010702201e746d53213b3cccd4
goranaviani <goranaviani@gmail.com>
2022-09-18 13:04:51 +0200

    first steps

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -5,16 +5,14 @@ import {Link} from "react-router-dom";
 import './Navigation.css'
 import LoginRegister from "../LoginRegister/LoginRegister";
 
-
+//             <Link to="/dashboard">Dashboard</Link>
 const Navigation = ({setSearchedCityDetails}) => {
     return (
         <div className="navigation">
             <Link to="/" onClick={() => {
                 setSearchedCityDetails(null)
-            }}>Index
+            }}>Index </Link>
 
-                <Link to="/dashboard">Dashboard</Link>
-            </Link>
             <LoginRegister/>
 
         </div>
