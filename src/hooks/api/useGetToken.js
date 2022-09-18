cf0899aa9c45d854da2c40e06e1d16e9a7acbe56
goranaviani <goranaviani@gmail.com>
2022-09-18 14:40:20 +0200

    first steps

--- src/hooks/api/useGetToken.js
+++ src/hooks/api/useGetToken.js
@@ -3,14 +3,13 @@ const FormData = require("form-data");
 const useGetToken = (axiosClient, setAccessKey) => {
 
     const data = new FormData();
-    data.append('username', process.env.WEATHER_USERNAME);
-    data.append('password', process.env.WEATHER_PASSWORD);
+    data.append('username', process.env.REACT_APP_WEATHER_USERNAME);
+    data.append('password', process.env.REACT_APP_WEATHER_PASSWORD);
     const config = {
         method: 'post',
         url: '/token',
         data: data
     };
-
     axiosClient(config)
         .then(function (response) {
             setAccessKey(response.data.access_token)
