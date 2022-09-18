4d6a60398fc7b6732bd25c4cf36a0b0975aa0647
goranaviani <goranaviani@gmail.com>
2022-09-18 13:27:11 +0200

    first steps

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -4,14 +4,22 @@ import {Link} from "react-router-dom";
 
 import './Navigation.css'
 import LoginRegister from "../LoginRegister/LoginRegister";
+import {Button} from "@mui/material";
 
 //             <Link to="/dashboard">Dashboard</Link>
 const Navigation = ({setSearchedCityDetails}) => {
     return (
         <div className="navigation">
-            <Link to="/" onClick={() => {
+            
+            <Button
+                variant="contained"
+                color="primary"
+                component={Link}
+                to="/" onClick={() => {
                 setSearchedCityDetails(null)
-            }}>Index </Link>
+            }}
+            >Index
+            </Button>
 
             <LoginRegister/>
 
