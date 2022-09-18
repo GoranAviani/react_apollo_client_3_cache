74e29768d4482f39caef1d52e1e21c9fb268a5a0
goranaviani <goranaviani@gmail.com>
2022-09-18 14:51:44 +0200

    first steps

--- src/components/LoginRegister/LoginRegister.js
+++ src/components/LoginRegister/LoginRegister.js
@@ -3,20 +3,25 @@ import {Link} from "react-router-dom";
 import LoginIcon from '@mui/icons-material/Login';
 import './LoginRegister.css'
 import {Button} from "@mui/material";
+import '../../css/Button.css'
 
 const LoginRegister = () => {
 
     return (
         <div className="loginregister">
-            <Button
-                variant="contained"
-                color="primary"
-                component={Link}
-                to="/login"
-                endIcon={<LoginIcon/>}
-                size="small"
-            >Login
-            </Button>
+            <div className="button-small center">
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    component={Link}
+                    to="/login"
+                    endIcon={<LoginIcon/>}
+                    size="small"
+                >Login
+                </Button>
+            </div>
+
         </div>
     )
 }
