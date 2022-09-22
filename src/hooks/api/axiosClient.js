81341bde7675ba5c0919051c6945e603f342e762
goranaviani <goranaviani@gmail.com>
2022-09-22 21:01:41 +0200

    first steps

--- src/hooks/api/axiosClient.js
+++ src/hooks/api/axiosClient.js
@@ -1,18 +1,16 @@
 import axios from "axios";
 
-let BASE_URL = ""
-const ENVIRONMENT_TYPE = process.env.ENVIRONMENT_TYPE
-
-if (ENVIRONMENT_TYPE === "dev") {
-    BASE_URL = "http://127.0.0.1:8000/"
-} else {
-    BASE_URL = "https://weather.deta.dev"
-}
 
 const createAxiosClient = () => {
-
-    console.log({BASE_URL})
+    let BASE_URL = ""
+    const REACT_APP_ENVIRONMENT_TYPE = process.env.REACT_APP_ENVIRONMENT_TYPE
+    console.log({REACT_APP_ENVIRONMENT_TYPE})
+    if (REACT_APP_ENVIRONMENT_TYPE === "dev") {
+        BASE_URL = "http://127.0.0.1:8000/"
+    } else {
+        BASE_URL = "https://weather.deta.dev"
+    }
     return axios.create({baseURL: BASE_URL})
 }
 
-export default createAxiosClient();
+export default createAxiosClient;
