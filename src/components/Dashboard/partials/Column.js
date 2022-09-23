27a7dc8f9ba595a4638d463bd9d95d242ce69747
goranaviani <goranaviani@gmail.com>
2022-09-23 22:42:15 +0200

    first steps

--- src/components/Dashboard/partials/Column.js
+++ src/components/Dashboard/partials/Column.js
@@ -2,36 +2,37 @@ import React from 'react'
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import Row from "./Row";
+import Grid from '@mui/material/Grid'; // Grid version 1
+import {Draggable} from 'react-beautiful-dnd';
+
 
 const Column = ({key, column, tasks}) => {
     console.log({tasks})
     return (
-        <Box
-            sx={{
-                width: 300,
-                height: 300,
-                backgroundColor: 'primary.dark',
-                '&:hover': {
-                    backgroundColor: 'primary.main',
-                    opacity: [0.9, 0.8, 0.7],
-                },
-            }}
-        ><Typography align='center' variant="h6">
-            {column.title}
-        </Typography>
+        <Draggable draggableId={taska.id} index={index}>
+            {(provided) => (
+                <Grid xs={4} {...provided.draggableProps}
+                      {...provided.draggableProps}
+                      ref={provided.ref}>
 
-            <Box inputRef={provided.ref}
-                 {...provided.droppableProps}>
-                {tasks.map((task, index) => {
-                    return <Row key={task.id} taska={task} index={index}/>
-                })}
-                {provided.placeholder}
+                    <Typography align='center' variant="h6">
+                        {column.title}
+                    </Typography>
 
-            </Box>
-            }
+                    <Item inputRef={provided.ref}
+                          {...provided.droppableProps}>
+                        {tasks.map((task, index) => {
+                            return <Row key={task.id} taska={task} index={index}/>
+                        })}
+                        {provided.placeholder}
+
+                    </Item>
+                    }
 
 
-        </Box>
+                </Grid>
+            )}</Draggable>
+
     )
 }
 
