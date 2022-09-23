4b684551c5c02bd04b7a98323c1552a55265f1e4
goranaviani <goranaviani@gmail.com>
2022-09-23 22:42:19 +0200

    first steps

--- src/components/Dashboard/partials/Row.js
+++ src/components/Dashboard/partials/Row.js
@@ -1,23 +1,12 @@
 import React from 'react'
 import Typography from "@mui/material/Typography";
-import {Draggable} from 'react-beautiful-dnd';
 import Box from '@mui/material/Box';
 
 const Row = ({key, taska, index}) => {
     return (
-        <Draggable draggableId={taska.id} index={index}>
-            {(provided) => (
-                <Box {...provided.draggableProps}
-                     {...provided.draggableProps}
-                     inputRef={provided.ref}>
-
-                    {taska.content}
-
-                </Box>
-            )}
-
-        </Draggable>
-
+        < >
+            {taska.content}
+        </>
     )
 }
 
