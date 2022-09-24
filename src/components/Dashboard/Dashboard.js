8c6ffb5f2c24e062fd8d35fbd5d60e0eab188176
goranaviani <goranaviani@gmail.com>
2022-09-24 15:08:26 +0200

    first steps

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -7,9 +7,35 @@ import {DragDropContext} from 'react-beautiful-dnd';
 const Dashboard = () => {
     const [dbColumns, setDbColumns] = useState(backendColumns)
 
-    const onDragEnd = (result, columns, setDbColumns) => {
-        console.log({result})
-        console.log({columns})
+    const onDragEnd = (result, dbColumns, setDbColumns) => {
+
+        if (!result.destination) return;
+        const {source, destination} = result;
+        console.log({source})
+        console.log({destination})
+
+        // checking if droppable id done belong to same column
+        if (source.droppableId !== destination.droppableId) {
+            const startingColumn = dbColumns[source.droppableId];
+            const destinationColumn = dbColumns[destination.droppableId];
+            const startingColumnItems = [...startingColumn.items];
+            const destinationColumnItems = [...destinationColumn.items];
+            const [removed] = startingColumnItems.splice(source.index, 1);
+            destinationColumnItems.splice(destination.index, 0, removed);
+            setDbColumns({
+                ...dbColumns,
+                [source.droppableId]: {
+                    ...startingColumn,
+                    items: startingColumnItems
+                },
+                [destination.droppableId]: {
+                    ...destinationColumn,
+                    items: destinationColumnItems
+                }
+            });
+        }
+
+
     }
 
 
