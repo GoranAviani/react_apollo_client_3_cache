a27bfc6e35f66f20c755658c5c9891fc498f5eb8
goranaviani <goranaviani@gmail.com>
2022-09-24 17:45:44 +0200

    changing keys

--- src/components/WeatherBoard/static/backendData.js
+++ src/components/WeatherBoard/static/backendData.js
@@ -1,7 +1,6 @@
-
 const backendData = [
-    {id: 123, cityName: "Stockholm"},
-    {id: 991239, cityName: "Split"},
+    {id: '123', cityName: "Stockholm"},
+    {id: '991239', cityName: "Split"},
     {id: 'asdada', cityName: "London"},
     {id: 'adodkasod', cityName: "New York"},
     {id: '01203kd01', cityName: "Tokyo"}
