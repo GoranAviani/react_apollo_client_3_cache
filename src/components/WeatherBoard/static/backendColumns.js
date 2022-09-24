01ff980a9e1ce6a01cde5790a4870a93aaf7ebd4
goranaviani <goranaviani@gmail.com>
2022-09-24 17:44:07 +0200

    first stepschanging backend columns keys

--- src/components/WeatherBoard/static/backendColumns.js
+++ src/components/WeatherBoard/static/backendColumns.js
@@ -1,15 +1,15 @@
 import backendData from "./backendData";
 
 const backendColumns = {
-    [1]: {
+    1: {
         name: "1",
         items: backendData
     },
-    [2]: {
+    2: {
         name: "2",
         items: []
     },
-    [3]: {
+    3: {
         name: "3",
         items: []
     }
