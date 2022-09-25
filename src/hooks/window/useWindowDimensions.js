b8325c60612064aa3f07f54dde89c77644ae2aa7
goranaviani <goranaviani@gmail.com>
2022-09-25 14:40:20 +0200

    buidling the dashboard demo

--- src/hooks/window/useWindowDimensions.js
+++ src/hooks/window/useWindowDimensions.js
@@ -1,32 +1,32 @@
-import { useState, useEffect } from 'react';
+import {useState, useEffect} from 'react';
 
 const getWindowDimensions = () => {
-  const { innerWidth: width, innerHeight: height } = window;
-  if (width > 600){
-    return 'desktop'
-  }
-  else{
-    return 'mobile'
-  }
-  // return {
-   // width,
-  //  height
- // };
+    const {innerWidth: width, innerHeight: height} = window;
+    if (width > 600) {
+        return 'desktop'
+    } else {
+        return 'mobile'
+    }
+    // return {
+    // width,
+    //  height
+    // };
 }
 
-const useWindowDimensions =() => {
-  const [windowDimensions, setWindowDimensions] = useState(useWindowDimensions());
+const useWindowDimensions = () => {
+    const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
-  useEffect(() => {
-    function handleResize() {
-      setWindowDimensions(getWindowDimensions());
-    }
+    useEffect(() => {
+        function handleResize() {
+            setWindowDimensions(getWindowDimensions());
+        }
 
-    window.addEventListener('resize', handleResize);
-    return () => window.removeEventListener('resize', handleResize);
-  }, []);
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
 
-  return windowDimensions;
+    console.log({windowDimensions})
+    return windowDimensions;
 }
 
 export default useWindowDimensions
