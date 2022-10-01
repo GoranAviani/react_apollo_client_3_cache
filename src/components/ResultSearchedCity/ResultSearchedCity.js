b9e779eb5a53b881f3ef7038089ff83dd7acc1c2
goranaviani <goranaviani@gmail.com>
2022-10-01 11:10:51 +0200

    fixing search sizings

--- src/components/ResultSearchedCity/ResultSearchedCity.js
+++ src/components/ResultSearchedCity/ResultSearchedCity.js
@@ -14,39 +14,39 @@ const ResultSearchedCity = ({searchedCityDetails}) => {
         <div className="resultSearchedCity">
             <div className="citsearchedCityInfoName">
                 <Typography variant="h5">{searchedCityDetails.data.location.name}</Typography>
-
             </div>
-            <div className="searchedCityMainDetails">
-                <div className="currentTemperature">
+            <div className="searchedCityAllDetails">
+                <div className="searchedCityMainDetails">
+                    <div className="currentTemperature">
+                        <div className="currentTemperatureTitle">Temperature</div>
+                        <div className="currentTemperatureNumber">{} </div>
+                    </div>
 
-                    <div className="currentTemperatureTitle">Temperature</div>
-                    <div className="currentTemperatureNumber">{} </div>
+                    <div>
+                        <div className="currentWeatherIcon">weather Icon</div>
+                        <Card>
+                            <CardMedia
+                                component="img"
+                                image={searchedCityDetails.data.current.condition.icon}
+                                alt={searchedCityDetails.data.current.condition.text}
+                            />
+                        </Card>
+                    </div>
                 </div>
+                <div className="searchedCityOtherDetails">
+                    <div>
+                        <div className="lowestTemperatureTitle">Daily lowest</div>
+                        <div className="lowestTemperatureNumber">{searchedCityDetails.data.cityName}</div>
+                    </div>
+                    <div>
+                        <div className="highestTemperatureTitle">Daily highest</div>
+                        <div className="highestTemperatureNumber">****</div>
+                    </div>
 
-                <div>
-                    <div className="currentWeatherIcon">weather Icon</div>
-                    <Card>
-                        <CardMedia
-                            component="img"
-                            image={searchedCityDetails.data.current.condition.icon}
-                            alt={searchedCityDetails.data.current.condition.text}
-                        />
-                    </Card>
-                </div>
-            </div>
-            <div className="searchedCityOtherDetails">
-                <div>
-                    <div className="lowestTemperatureTitle">Daily lowest</div>
-                    <div className="lowestTemperatureNumber">{searchedCityDetails.data.cityName}</div>
-                </div>
-                <div>
-                    <div className="highestTemperatureTitle">Daily highest</div>
-                    <div className="highestTemperatureNumber">****</div>
-                </div>
-
-                <div>
-                    <div className="weatherDescriptionTitle">Weather description</div>
-                    <div className="weatherDescriptionText">{searchedCityDetails.weatherNow}</div>
+                    <div>
+                        <div className="weatherDescriptionTitle">Weather description</div>
+                        <div className="weatherDescriptionText">{searchedCityDetails.weatherNow}</div>
+                    </div>
                 </div>
             </div>
         </div>
