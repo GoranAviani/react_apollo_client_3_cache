99f9cea2f9eab3b9d3138ca9e798412fe62435a5
goranaviani <goranaviani@gmail.com>
2022-10-15 16:57:02 +0200

    initiating project

--- src/apollo/cache/queries/totoList.js
+++ src/apollo/cache/queries/totoList.js
@@ -3,9 +3,10 @@ import {gql} from '@apollo/client';
 export const TODO_LIST = gql`
   query TodoList($id: ID!) {
     data(id: $id) {
-      id
-      text
-      completed
+      name
+      lastname
+      email
+      address
     }
   }
 `;
