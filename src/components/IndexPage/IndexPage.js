82957eed4471c081f0cc383cf70875a1e3c6f4b4
goranaviani <goranaviani@gmail.com>
2022-10-15 17:26:18 +0200

    initiating project

--- src/components/IndexPage/IndexPage.js
+++ src/components/IndexPage/IndexPage.js
@@ -29,6 +29,7 @@ const IndexPage = () => {
         })
         console.log({userData1})
         console.log(`user name: ${userData1.userDataCache.name}`)
+
         client.writeQuery({
             query: USER_DATA, data: {
                 userDataCache: {
